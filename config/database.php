ac8c7fd880037633c7e1318cfd1c13fef199f2fe
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-05-07 02:28:59 +0300

    cleare up on the project, proper database configuration and cleared on the index.php for form submision

--- config/database.php
+++ config/database.php
@@ -4,9 +4,9 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'feedbackApp');
 //Creating the database connection
-$connect= new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+$conn= new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 //Check connection
-if($connect->connect_error){
-    die('Connection Failed '.$connect->connect_error);
+if($conn->connect_error){
+    die('Connection Failed '.$conn->connect_error);
 }
 //echo 'Connection Successful'; //testing connetion
